e19123e7fabf835348da1528c8ac324bd4427c05
Ramil Muhametdinov <rami-m@mail.ru>
2017-02-05 22:36:26 +0400

    init

---
+++ src/test/java/DemoFailedTest.java
@@ -0,0 +1,39 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.AuthPage;
+import pageObjects.MainPage;
+import pageObjects.RequestsPage;
+import ru.yandex.qatools.allure.annotations.Features;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.annotations.Title;
+import utils.Utils;
+
+/**
+ * Created by Administrator on 12/26/2016.
+ */
+@Features("Тестирование ЕВРАЗ")
+public class DemoFailedTest extends TestInit{
+    Utils util;
+
+    @Stories("Демонстрационные тесты")
+    @Title("ДемоТест1")
+    @Test
+    void testBody() {
+        AuthPage authPage = new AuthPage(driver);
+        authPage.doLogin();
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.selectMenus();
+        Assert.fail("Принудительная ошибка теста");
+        mainPage.clickToRequestsElem();
+
+        RequestsPage requestsPage = new RequestsPage(driver);
+        requestsPage.addRequest();
+
+        mainPage.openNewReq();
+
+        requestsPage.getToWork();
+
+        mainPage.remove();
+    }
+}
